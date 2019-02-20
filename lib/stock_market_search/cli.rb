55220e91e4816f362e9967bd075f6b16d79dfae6
Alexandra Palermo <alexmpalermo@gmail.com>
2019-02-20 17:56:29 +0000

    modified call mathod. added choices and choose methods

--- lib/stock_market_search/cli.rb
+++ lib/stock_market_search/cli.rb
@@ -4,37 +4,22 @@ class StockMarketSearch::CLI
     puts "Welcome to Stock Market Search!"
     puts "Please enter a ticker symbol (e.g. MCD)"
     input = gets.strip
+    
+    company_hash = {
+      :ticker => input 
+    }
+    
     url = "https://www.etrade.wallst.com/v1/stocks/snapshot/snapshot.asp?symbol=#{input}" if input.match(/[a-zA-Z]/)
     
     StockMarketSearch::Company.new_by_url(url)
+    
     puts "You have chosen: "StockMarketSearch::Company.name
-    puts "What would you like to do? Please choose a number 1-4."
-    puts "1. View current market price"
-    puts "2. Company overview"
-    puts "3. View charts/buy or sell shares on Etrade"
-    puts "4. Exit"
-    input = gets.strip
-    if input == "1"
-      puts "Market Price: "StockMarketSearch::Company.price 
-      elsif input == "2"
-      puts "Overview: "StockMarketSearch::Company.overview 
-      puts "Location: "StockMarketSearch::Company.location
-      puts "Website url: "StockMarketSearch::Company.website
-      elsif input == "3"
-      puts "Charts link: "StockMarketSearch::Company.charts_url
-    elsif input == "4"
-    ------exit ??
-  else 
-    "Invalid number. Please choose a number 1-4."
+    
+  choose until input == "4"
+    
   end
 end 
     
-    
-    input_url(input)
-    
-    puts "You have chosen: "StockMarketSearch::Company.name 
-    
-  end 
   
   def input_url(input)
     if input.match(/[a-zA-Z]/)
@@ -46,7 +31,30 @@ end
 end
 end 
 
-
+  def choices
+    puts "What would you like to do? Please choose a number 1-4."
+    puts "1. View current market price"
+    puts "2. Company overview"
+    puts "3. View charts/buy or sell shares on Etrade"
+    puts "4. Exit"
+  end 
   
+  def choose
+    choices 
+    input= gets.strip
+    if input == "1"
+      puts "Market Price: "StockMarketSearch::Company.price
+    elsif input == "2"
+      puts "Overview: "StockMarketSearch::Company.overview 
+      puts "Location: "StockMarketSearch::Company.location
+      puts "Website url: "StockMarketSearch::Company.website
+    elsif input == "3"
+      puts "Charts link: "StockMarketSearch::Company.charts_url
+    elsif input == "4"
+  else 
+    "Invalid number. Please choose a number 1-4."
+  end 
+end 
+    
   
 end
