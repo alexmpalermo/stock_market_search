91f9e0a404f9be745da723a2c3345464ba4ca953
Alexandra Palermo <alexmpalermo@gmail.com>
2019-02-20 16:50:24 +0000

    modified call method with two puts and gets. added input_url method

--- lib/stock_market_search/cli.rb
+++ lib/stock_market_search/cli.rb
@@ -1,5 +1,19 @@
 class StockMarketSearch::CLI 
+  
   def call 
-    puts "Enter a ticker symbol"
+    puts "Welcome to Stock Market Search!"
+    puts "Please enter a ticker symbol (e.g. MCD)"
+    input = gets.strip
+    
   end 
+  
+  def input_url(input)
+    if input.match(/[a-zA-Z]/)
+    url = "https://www.etrade.wallst.com/v1/stocks/snapshot/snapshot.asp?symbol=#{input}"
+  else 
+  puts "The system doesn't recognize that, please enter a ticker symbol (e.g. MCD)"
+end
+end 
+  
+  
 end
