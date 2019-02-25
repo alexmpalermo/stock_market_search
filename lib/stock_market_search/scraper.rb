26e04659a66dd1201a0a726e057d411602963d22
Alexandra Palermo <alexmpalermo@gmail.com>
2019-02-25 19:39:55 +0000

    fixed market price in scraper and name

--- lib/stock_market_search/scraper.rb
+++ lib/stock_market_search/scraper.rb
@@ -2,12 +2,13 @@ class StockMarketSearch::Scraper
   
   def self.scrape_url(url)
     doc = Nokogiri::HTML(open(url))
+    #binding.pry 
     company_hash = {
       :name => doc.search("h1.company__name").text, 
-      #:overview => doc.search("div.lh18").text,
+      #:overview => doc.search("p.description__text").text,
       #:location => doc.search("span.fRight").text,
       #:website => doc.search("span.fRight a").first["href"],
-      :price => doc.search("bg_quote.value positive").text,
+      :price => doc.search("h3.intraday__price bg-quote").text,
       :etrade_url => url
     }
     company_hash
