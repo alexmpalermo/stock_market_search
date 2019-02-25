6e6953704344f590076c83b5a3d8b625c53beab6
Alexandra Palermo <alexmpalermo@gmail.com>
2019-02-25 20:11:19 +0000

    fixed overview

--- lib/stock_market_search/scraper.rb
+++ lib/stock_market_search/scraper.rb
@@ -2,10 +2,10 @@ class StockMarketSearch::Scraper
   
   def self.scrape_url(url)
     doc = Nokogiri::HTML(open(url))
-    #binding.pry 
+    binding.pry 
     company_hash = {
       :name => doc.search("h1.company__name").text, 
-      :overview => doc.search("p.description__text").text.strip,
+      :overview => overview = doc.search('p.description__text> text()').text.strip,
       :price_update => doc.search("div.intraday__timestamp").text,
       :price => doc.search("h3.intraday__price bg-quote").text,
       :mktwatch_url => url
